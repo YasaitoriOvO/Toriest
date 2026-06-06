6766a49b2bbf8c30861ca60b7302c284baaf2ed6
Yasatori <caijidayo@outlook.com>
2026-06-07 01:22:53 +0800

    feat: add new link for Nekro’s SEKAI

--- src/pages/link.vue
+++ src/pages/link.vue
@@ -44,7 +44,8 @@
     { icon: '/images/friends/akutazehy.jpeg', name: 'Akuta Zehy', desc: 'https://akutazehy.xyz/', links: 'https://akutazehy.xyz/'},
     { icon: '/images/friends/cutelittlesky.png', name: 'CuteLittleSky', desc: 'https://cutelittlesky.cn/', links: 'https://cutelittlesky.cn/' },
     { icon: '/images/friends/peter.jpeg', name: '朝仓 彩玲', desc: '火星上面有什么？', links: 'https://mygo.plus' }, 
-    { icon: 'https://carolyn.sh/img/avatar.png', name: 'Carolyn Sun', desc: 'Where Carolyn Lives', links: 'https://carolyn.sh'}
+    { icon: 'https://carolyn.sh/img/avatar.png', name: 'Carolyn Sun', desc: 'Where Carolyn Lives', links: 'https://carolyn.sh'},
+    {icon: 'https://avatars.githubusercontent.com/u/90670998?v=4', name:'Nekro’s SEKAI', desc:'自留地、日常记录、经验分享。', links:'https://www.nekro.top/'}
   ]
 </script>
 
